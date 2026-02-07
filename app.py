94d85fcdfb03b657174bf0752f2c61630d4e60dc
Sarath <skbare12345@gmail.com>
2026-02-07 14:16:53 +0530

    Initial dockerized flask app

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+ENV = os.getenv("APP_ENV", "prod")
+
 @app.route("/")
 def home():
-    return "🚀  	im sarath"
+    return f"🚀 Running in {ENV.upper()} mode"
+@app.route("/crash")
+def crash():
+    return 1 / 0
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
-
+    debug = True if ENV == "dev" else False
+    app.run(host="0.0.0.0", port=5000, debug=debug)
